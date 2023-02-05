2378d8a9e8a637061ab27118a8dc68d453672697
DMITRY <bdodinka1992@gmail.com>
2023-02-05 19:13:05 +0300

    feat:add wrapper component

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
         "plugin:prettier/recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
+        "prettier",
         "next/core-web-vitals",
     ],
     parserOptions: {
---
+++ src/components/wrapper.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from '../styles/components/wrapper.module.scss'
+
+const Wrapper: React.FC<{ children: React.ReactNode, className?: string }> = ({children, className}) => {
+
+    const finalClass = className ? `${style.wrapper} ${className}` : style.wrapper
+
+    return (
+        <section className={finalClass}>
+            {children}
+        </section>
+    );
+};
+
+export default Wrapper;
