629849fec8890e0e7338a301287e51e0976a3bc3
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-06 20:33:29 +0300

    fix: style to logo

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -3,24 +3,24 @@ import Link from "next/link";
 import style from '../../../styles/components/header.module.scss'
 
 const Header: React.FC<{ isMainPage: boolean, currentPage: string }> = ({isMainPage, currentPage}) => {
-    const headerStyle = isMainPage ? style.header : `${style.header} ${style.headerMain}`
 
     return (
-        <header className={headerStyle}>
-            <div className={style.logoBlock}>
-                <div className={style.logo}>
+        <header className={style.header}>
+            <div className={style.row}>
+                <div className={style.logoBlock}>
                     <img src='/Logo.svg' alt="silevans"/>
                 </div>
-                <span className={style.currentPage}>Главная <span>&#8226;</span> {currentPage}</span>
-            </div>
 
-            <div className={style.navBlock}>
-                <nav className={style.navLinks}>
-                    <Link href='/services'>Услуги</Link>
-                    <Link href='/portfolio'> Портфолио</Link>
-                </nav>
-                <div className={style.callbackBtn}>Заказать звонок</div>
+                <div className={style.navBlock}>
+                    <nav>
+                        <Link href='/services'>Услуги</Link>
+                        <Link href='/portfolio'> Портфолио</Link>
+                    </nav>
+                    <div className={style.callbackBtn}>Заказать звонок</div>
+                </div>
             </div>
+            {isMainPage && <span className={style.currentPage}>Главная &#8226; {currentPage}</span>}
+
 
         </header>
     );
