f0c8e151ec826ce5c5d55992708af79dcd891425
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-07 01:24:44 +0300

    fix:  background img for top component

--- src/components/topComponent/TopComponent.tsx
+++ src/components/topComponent/TopComponent.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Image from "next/image";
 import ground from "@/assets/svg/ground.svg";
-import FullButton from "@/components/common/buttons/FullButton";
 import Header from "@/components/common/header/Header";
 import Wrapper from "@/components/common/wrapper";
 import style from '../../styles/components/topComponent.module.scss'
 
+
 const TopComponent = () => {
     return (
         <Wrapper className={style.wrapper}>
-            <Image src={ground} alt={"ground"} className={style.ground}/>
+            <Image src={ground} alt={"ground"} width={1440} height={770} className={style.ground}/>
             <Header isMainPage={true} currentPage={"Разработка сайтов"}/>
-            <FullButton className={style.button}>Заполнить анкету</FullButton>
+           
 
         </Wrapper>
     );
