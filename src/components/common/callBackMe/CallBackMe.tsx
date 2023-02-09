8508956f8546d02a52d152060075d12a56d0c98d
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-09 16:57:42 +0300

    feat: added css to form, deleted border in input, textarea

--- src/components/common/callBackMe/CallBackMe.tsx
+++ src/components/common/callBackMe/CallBackMe.tsx
@@ -10,11 +10,15 @@ const CallBackMe = () => {
             <SmallTitle children={"Остались вопросы?"} className={style.title}/>
             <div className={style.form}>
                 <div className={style.column}>
-                    <div className={style.name}>name</div>
+                    <input type="text" placeholder="Иванов Иван Иванович"
+                           className={style.name}
+                    />
                     <p className={style.description}>Оставьте свои контакты и наши специалисты с вами свяжутся</p>
                 </div>
                 <div className={style.column}>
-                    <div className={style.phone}>+37544 5590660</div>
+                    <input type="tel" placeholder="+7 (___) ___ __ __"
+                           className={style.phone}
+                    />
                     <FullButton>Заказать звонок</FullButton>
                 </div>
             </div>
