7f791968d65b927d4cebdccb9d2fd6700f260ce8
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-09 12:55:38 +0300

    feat: added form into callbackMe component

--- src/components/common/callBackMe/CallBackMe.tsx
+++ src/components/common/callBackMe/CallBackMe.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import SmallTitle from "@/components/common/Titles/SmallTitle";
+import Wrapper from "@/components/common/wrapper";
+import style from './../../../styles/components/callBackMe.module.scss'
+import FullButton from "@/components/common/buttons/FullButton";
 
 const CallBackMe = () => {
     return (
-        <div>
-            <SmallTitle children={"Остались вопросы?"}/>
-        </div>
+        <Wrapper className={style.wrapper}>
+            <SmallTitle children={"Остались вопросы?"} className={style.title}/>
+            <div className={style.form}>
+                <div className={style.column}>
+                    <div className={style.name}>name</div>
+                    <p className={style.description}>Оставьте свои контакты и наши специалисты с вами свяжутся</p>
+                </div>
+                <div className={style.column}>
+                    <div className={style.phone}>+37544 5590660</div>
+                    <FullButton>Заказать звонок</FullButton>
+                </div>
+            </div>
+        </Wrapper>
     );
 };
 
