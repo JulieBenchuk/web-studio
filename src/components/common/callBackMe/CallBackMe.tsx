effd29aac1869d9dafdbdacdfa1a499e88690f4d
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-11 13:51:10 +0300

    feat: added formik, yup to callbackme component

--- src/components/common/callBackMe/CallBackMe.tsx
+++ src/components/common/callBackMe/CallBackMe.tsx
@@ -3,25 +3,42 @@ import SmallTitle from "@/components/common/Titles/SmallTitle";
 import Wrapper from "@/components/common/wrapper";
 import style from './../../../styles/components/callBackMe.module.scss'
 import FullButton from "@/components/common/buttons/FullButton";
+import {useFormik} from "formik";
+import * as yup from "yup";
 
 const CallBackMe = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            phone: "",
+        },
+        onSubmit: (values, {resetForm}) => {
+            console.log(values.name, values.phone)
+            resetForm()
+        },
+        validationSchema: yup.object({
+            name: yup.string().trim().required("Необходимо ввести имя"),
+            phone: yup.string().required("Необходима ввести телефон").min(11)
+        })
+    })
     return (
         <Wrapper className={style.wrapper}>
             <SmallTitle children={"Остались вопросы?"} className={style.title}/>
-            <div className={style.form}>
+            <form className={style.form} onSubmit={formik.handleSubmit}>
                 <div className={style.column1}>
-                    <input type="text" placeholder="Иванов Иван Иванович"
-                           className={style.name}
+                    <input name={"name"} type="text" value={formik.values.name} placeholder="Иванов Иван Иванович"
+                           className={style.name} onChange={formik.handleChange} onBlur={formik.handleBlur}
                     />
                     <p className={style.description}>Оставьте свои контакты и наши специалисты с вами свяжутся</p>
                 </div>
                 <div className={style.column2}>
-                    <input type="tel" placeholder="+7 (___) ___ __ __"
-                           className={style.phone}
+                    <input type="tel" name={"phone"} value={formik.values.phone} placeholder="+7 (___) ___ __ __"
+                           className={style.phone} onChange={formik.handleChange} onBlur={formik.handleBlur}
                     />
-                    <FullButton className={style.button}>Заказать звонок</FullButton>
+                    <FullButton className={style.button} type={"submit"}>Заказать звонок</FullButton>
                 </div>
-            </div>
+            </form>
         </Wrapper>
     );
 };
