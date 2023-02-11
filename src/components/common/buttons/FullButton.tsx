85d8e7a3ff1bc66d236f23ca35c69626acb5b0c5
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-11 15:02:54 +0300

    feat: added formik errors (handlers & style), deleted Rectangle .png

--- src/components/common/buttons/FullButton.tsx
+++ src/components/common/buttons/FullButton.tsx
@@ -2,12 +2,12 @@ import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react';
 import style from '../../../styles/components/buttons/buttons.module.scss'
 
 const FullButton: React.FC<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>> = ({className, children, ...restProps}) => {
+    HTMLButtonElement>> = ({className, children, type, ...restProps}) => {
 
     const finalClass = className ? `${style.default} ${className}` : style.default
 
     return (
-        <button className={finalClass}>{children}</button>
+        <button className={finalClass} type={type}>{children}</button>
     );
 };
 
--- src/components/common/callBackMe/CallBackMe.tsx
+++ src/components/common/callBackMe/CallBackMe.tsx
@@ -19,25 +19,32 @@ const CallBackMe = () => {
         },
         validationSchema: yup.object({
             name: yup.string().trim().required("Необходимо ввести имя"),
-            phone: yup.string().required("Необходима ввести телефон").min(11)
+            phone: yup.string().required("Необходима ввести телефон").min(11, "Слишком короткий номер")
         })
     })
     return (
         <Wrapper className={style.wrapper}>
             <SmallTitle children={"Остались вопросы?"} className={style.title}/>
             <form className={style.form} onSubmit={formik.handleSubmit}>
+
                 <div className={style.column1}>
+                    {formik.errors.name && formik.touched.name &&
+                        <span className={style.formikError}>{formik.errors.name}</span>}
                     <input name={"name"} type="text" value={formik.values.name} placeholder="Иванов Иван Иванович"
                            className={style.name} onChange={formik.handleChange} onBlur={formik.handleBlur}
                     />
                     <p className={style.description}>Оставьте свои контакты и наши специалисты с вами свяжутся</p>
                 </div>
+
                 <div className={style.column2}>
+                    {formik.errors.phone && formik.touched.phone &&
+                        <span className={style.formikError}>{formik.errors.phone}</span>}
                     <input type="tel" name={"phone"} value={formik.values.phone} placeholder="+7 (___) ___ __ __"
                            className={style.phone} onChange={formik.handleChange} onBlur={formik.handleBlur}
                     />
                     <FullButton className={style.button} type={"submit"}>Заказать звонок</FullButton>
                 </div>
+
             </form>
         </Wrapper>
     );
