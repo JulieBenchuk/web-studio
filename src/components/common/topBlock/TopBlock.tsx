b7e2ac702f7a1dcba9b6512914a09ef9d43e260a
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-13 23:16:31 +0300

    feat: style of top block

--- src/components/common/topBlock/TopBlock.tsx
+++ src/components/common/topBlock/TopBlock.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Wrapper from "@/components/common/wrapper";
 import Header from "@/components/common/header/Header";
 import style from "./../../../styles/components/topBlock.module.scss"
+import bgImage from "../../../assets/img/developmentTopBg.png"
+import MainTitle from "@/components/common/Titles/MainTitle";
 
 const TopBlock: React.FC<{}> = () => {
     return (
         <Wrapper className={style.wrapper}>
             <Header isMainPage={false} currentPage={"Разработка сайтов"}/>
+            <img src={bgImage.src} alt="background" className={style.bgImage}/>
+            <MainTitle title={"Разработка сайтов"}/>
+
 
         </Wrapper>
     );
