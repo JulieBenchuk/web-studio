35eed1fa0bdbc90face3a159aad15e6b2f6f7a57
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-13 21:24:12 +0300

    fix: added onClick to FullButton

--- src/components/common/buttons/FullButton.tsx
+++ src/components/common/buttons/FullButton.tsx
@@ -2,12 +2,12 @@ import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react';
 import style from '../../../styles/components/buttons/buttons.module.scss'
 
 const FullButton: React.FC<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>> = ({className, children, type, ...restProps}) => {
+    HTMLButtonElement>> = ({className, onClick, children, type, ...restProps}) => {
 
     const finalClass = className ? `${style.default} ${className}` : style.default
 
     return (
-        <button className={finalClass} type={type}>{children}</button>
+        <button className={finalClass} type={type} onClick={onClick}>{children}</button>
     );
 };
 
