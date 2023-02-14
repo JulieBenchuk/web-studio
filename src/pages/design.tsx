7bfd6d4c40aabfcdeb88315bb1e8593e41d695fb
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-14 17:37:04 +0300

    feat: added TopBlock to Design

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Head from "next/head";
+import style from "@/styles/Home.module.scss";
+import TopBlock from "@/components/common/topBlock/TopBlock";
+import bgImage from "@/assets/svg/designBg.svg";
 
 const Design: React.FC<{}> = () => {
     return (
-        <div>
-            Веб-дизайн
-        </div>
+        <>
+            <Head>
+                <title>Design</title>
+            </Head>
+            <main className={style.main}>
+                <TopBlock currentPage={"Веб-дизайн"}
+                          serviceDescription={"— имидж и стиль. Грамотная концепция формирует первое впечатление. Это всегда требует больше внимания"}
+                          backgroundImage={bgImage.src} button={"Заполнить анкету"}
+                          buttonDescription={"Заполните анкету, чтобы получить бесплатную консультацию"}
+                />
+            </main>
+        </>
     );
 };
 
