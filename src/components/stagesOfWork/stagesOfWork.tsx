6358bac1c2e8b94fb2a7bc352fbb62712ab04154
DMITRY <bdodinka1992@gmail.com>
2023-02-14 20:29:33 +0300

    fix:stageOfWork without adaptive

--- src/components/stagesOfWork/stagesOfWork.tsx
+++ src/components/stagesOfWork/stagesOfWork.tsx
@@ -8,21 +8,15 @@ import style from '../../styles/components/stagesOfWorks.module.scss'
 const StagesOfWork = () => {
     return (
         <Wrapper className={style.wrapper}>
+            <div className={style.titleBlock}>
+                <SmallTitle>Этапы работы</SmallTitle>
+                <p className={style.description}>
+                    {'Сроки этапов варьируются в зависимости\nот их объёма и сложности:'}
+                </p>
+            </div>
             {dataStagesDevelopment.map(el =>
-                el.id === '1'
-                    ? <div className={style.topBlock}>
-                        <div className={style.titleBlock}>
-                            <SmallTitle>Этапы работы</SmallTitle>
-                            <p className={style.description}>
-                                {'Сроки этапов варьируются в зависимости\nот их объёма и сложности:'}
-                            </p>
-                        </div>
-                        <StagesWork text={el.desc} title={el.title} number={Number(el.id)} key={el.id}/>
-                    </div>
-                    :
-                    <div className={style.items}>
-                        <StagesWork text={el.desc} title={el.title} number={Number(el.id)} key={el.id}/>
-                    </div>)}
+                <StagesWork text={el.desc} title={el.title} number={Number(el.id)} key={el.id}/>
+            )}
         </Wrapper>
     );
 };
