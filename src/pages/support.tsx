ea5123f6070cf65fa6fe53551d0930413f0b85d9
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-14 17:47:10 +0300

    feat: added top block to Support

---
+++ src/pages/support.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from "next/head";
+import style from "@/styles/Home.module.scss";
+import TopBlock from "@/components/common/topBlock/TopBlock";
+import bgImage from "@/assets/svg/supportBg.svg";
+
+const Support = () => {
+    return (
+        <>
+            <Head>
+                <title>Support</title>
+            </Head>
+            <main className={style.main}>
+                <TopBlock currentPage={"поддержка сайта"}
+                          serviceDescription={"— С гарантированным временем реакции и фиксированным сроком выполнения задач"}
+                          backgroundImage={bgImage.src} button={"Заполнить анкету"}
+                          buttonDescription={"Заполните анкету, чтобы получить бесплатную консультацию"}
+                />
+            </main>
+
+        </>
+    );
+};
+
+export default Support;
