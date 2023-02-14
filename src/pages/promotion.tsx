aedab683054f52c5b4d81ca167cdc867d1bcf20c
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-14 17:20:06 +0300

    feat: added TopBlock to Promotion

--- src/pages/promotion.tsx
+++ src/pages/promotion.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Head from "next/head";
+import style from "@/styles/Home.module.scss";
+import TopBlock from "@/components/common/topBlock/TopBlock";
+import bgImage from "@/assets/svg/promotionBg.svg";
 
 const Promotion: React.FC<{}> = () => {
     return (
-        <div>
-            Интернет продвижение
-        </div>
+        <>
+            <Head>
+                <title>Promotion</title>
+            </Head>
+            <main className={style.main}>
+                <TopBlock currentPage={"Интернет продвижение"}
+                          serviceDescription={"— Для быстрого привлечения ваших клиентов и увеличении конверсии"}
+                          backgroundImage={bgImage.src} button={"Заполнить анкету"}
+                          buttonDescription={"Заполните анкету, чтобы получить бесплатную консультацию"}
+                />
+            </main>
+
+        </>
     );
 };
 
