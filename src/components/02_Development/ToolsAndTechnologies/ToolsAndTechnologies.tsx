617732194f7bff50cbd23efd3fd69fea37eff716
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-16 01:04:55 +0300

    feat: added ToolsAndTechnologies component

---
+++ src/components/02_Development/ToolsAndTechnologies/ToolsAndTechnologies.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "@/styles/components/toolsAndTechnologies.module.scss"
+
+const ToolsAndTechnologies = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ToolsAndTechnologies;
