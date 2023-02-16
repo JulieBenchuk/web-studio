ff128d3e4facd8e91f4099988fe82ffbbde69ebd
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-16 22:26:58 +0300

    feat: added gears

--- src/components/02_Development/ToolsAndTechnologies/ToolsAndTechnologies.tsx
+++ src/components/02_Development/ToolsAndTechnologies/ToolsAndTechnologies.tsx
@@ -3,15 +3,21 @@ import style from "@/styles/components/toolsAndTechnologies.module.scss"
 import Wrapper from "@/components/common/wrapper";
 import spaceGif from "@/assets/gif/space.gif"
 import {toolItems} from "@/components/02_Development/ToolsAndTechnologies/ToolItems";
+import purpleGear from "@/assets/img/gears_icons/purpleGear.png"
+import pinkGear from "@/assets/img/gears_icons/roseGear.png"
+import blueBigGear from "@/assets/img/gears_icons/blueGear.png"
+import blueGear from "@/assets/img/gears_icons/blueGear2.png"
+import greenGear from "@/assets/img/gears_icons/greenGear.png"
+import greenBigGear from "@/assets/img/gears_icons/greenGear2.png"
 
 
 const ToolsAndTechnologies = () => {
     return (
         <Wrapper className={style.wrapper}>
-            <div className={style.gifBlockForMobile}>
-                <img src={spaceGif.src} alt={"tools"} className={style.gifForMobile}/>
-            </div>
             <div className={style.toolsInfoBlock}>
+                <div className={style.gifBlockForMobile}>
+                    <img src={spaceGif.src} alt={"tools"} className={style.gifForMobile}/>
+                </div>
                 <h4 className={style.titleRow}>Набор инструментов <span>и технологий</span></h4>
                 <div className={style.gifAndTextRow}>
                     <div className={style.gifBlock}>
@@ -67,8 +73,15 @@ const ToolsAndTechnologies = () => {
 
                     </div>
                 </div>
+                <div className={style.gears}>
+                    <img src={purpleGear.src} alt="tool" className={style.purple}/>
+                    <img src={pinkGear.src} alt="tool" className={style.pink}/>
+                    <img src={blueGear.src} alt="tool" className={style.blue}/>
+                    <img src={blueBigGear.src} alt="tool" className={style.blueBig}/>
+                    <img src={greenGear.src} alt="tool" className={style.green}/>
+                    <img src={greenBigGear.src} alt="tool" className={style.greenBig}/>
+                </div>
             </div>
-
         </Wrapper>
     );
 };
