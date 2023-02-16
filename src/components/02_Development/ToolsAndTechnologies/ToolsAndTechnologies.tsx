b9ee4297e8f3c13f53d5a21e2ff2804953fcfd64
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-16 14:51:36 +0300

    feat: added structure and some css to ToolsAndTechnologies component

--- src/components/02_Development/ToolsAndTechnologies/ToolsAndTechnologies.tsx
+++ src/components/02_Development/ToolsAndTechnologies/ToolsAndTechnologies.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import Image from "next/image";
 import style from "@/styles/components/toolsAndTechnologies.module.scss"
+import Wrapper from "@/components/common/wrapper";
+import spaceGif from "@/assets/gif/space.gif"
 
 const ToolsAndTechnologies = () => {
     return (
-        <div>
+        <Wrapper className={style.wrapper}>
+            <div className={style.toolsInfoBlock}>
+                <h4 className={style.titleRow}>Набор инструментов и технологий</h4>
+                <div className={style.gifAndTextRow}>
+                    <div className={style.gifBock}>
+                        <img src={spaceGif.src} alt={"tools"} className={style.gif}/>
+                    </div>
+                    <div className={style.text}>На этапе разработки мы добавляем в проект большую часть интерактивных и
+                        анимированных элементов, что позволяет получить работающие страницы и многократно протестировать
+                        их.
+                    </div>
+                </div>
 
-        </div>
+            </div>
+            <div className={style.technologyBlock}>
+                <div className={style.design}></div>
+                <div className={style.frontend}></div>
+                <div className={style.backend}></div>
+            </div>
+
+        </Wrapper>
     );
 };
 
