2bf2dbd670ac7e6a4c9b499182142f2f3c2267e3
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-17 17:34:02 +0300

    added: some css to WhoNeedPromotion

--- src/components/03_Promotion/WhoNeedPromotion/WhoNeedPromotion.tsx
+++ src/components/03_Promotion/WhoNeedPromotion/WhoNeedPromotion.tsx
@@ -3,6 +3,7 @@ import Wrapper from "@/components/common/wrapper";
 import style from "@/styles/components/whoNeedPromotion.module.scss"
 import SmallTitle from "@/components/common/Titles/SmallTitle";
 import FullButton from "@/components/common/buttons/FullButton";
+import girl from "@/assets/img/promotionGirl.png"
 
 const WhoNeedPromotion = () => {
     return (
@@ -16,20 +17,28 @@ const WhoNeedPromotion = () => {
                     <div className={style.topText}> Интернет можно сравнить с воскресной ярмаркой, на которой тысячи
                         продавцов наперебой предлагают свои товары и услуги.
                     </div>
-                    <div className={style.girlImg}>girlImg</div>
+                    <div className={style.girlImg}>
+                        <img src={girl.src} alt="promotion"/>
+                    </div>
                 </div>
 
                 <div className={style.nextPart}>
 
                     <div className={style.textBlock}>
-                        <span className={style.first}>В случае с цифровой торговлей такое место тоже есть — оно называется «топ поисковой выдачи». </span>
-                        <span className={style.second}>Все они знают: для того, чтобы торговля была успешной, нужно занять место, вокруг которого всегда толпятся покупатели.</span>
-                        <span className={style.third}>Мы знаем, как продвинуть ваш сайт в топ, привлечь покупателей и повысить продажи!</span>
+                        <div className={style.first}>В случае с цифровой торговлей такое место тоже есть — оно
+                            называется «топ поисковой выдачи».
+                        </div>
+                        <div className={style.second}>Все они знают: для того, чтобы торговля была успешной, нужно
+                            занять место, вокруг которого всегда толпятся покупатели.
+                        </div>
+                        <div className={style.third}>Мы знаем, как продвинуть ваш сайт в топ, привлечь покупателей и
+                            повысить продажи!
+                        </div>
                     </div>
 
                     <div className={style.buttonBlock}>
                         <span className={style.buttonDescription}>Заполните анкету, чтобы получить бесплатную консультацию</span>
-                        <FullButton>Заполнить анкету</FullButton>
+                        <FullButton className={style.button}>Заполнить анкету</FullButton>
                     </div>
 
                 </div>
