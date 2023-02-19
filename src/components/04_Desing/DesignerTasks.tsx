2adfe2aab187a59dfe77d8fe6595ab07a3debbf7
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-19 21:57:48 +0300

    feat: created DesignerTasks

---
+++ src/components/04_Desing/DesignerTasks.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Wrapper from "@/components/common/wrapper";
+import style from "@/styles/components/designerTasks.module.scss"
+
+const DesignerTasks = () => {
+    return (
+        <Wrapper className={style.wrapper}>
+
+        </Wrapper>
+    );
+};
+
+export default DesignerTasks;
