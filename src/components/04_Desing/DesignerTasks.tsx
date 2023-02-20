5be3b360cf66b1aa792bb1b9acae2e897639c491
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-20 13:55:57 +0300

    feat: pc version of DesignerTasks done

--- src/components/04_Desing/DesignerTasks.tsx
+++ src/components/04_Desing/DesignerTasks.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Wrapper from "@/components/common/wrapper";
 import style from "@/styles/components/designerTasks.module.scss"
+import Wrapper from "@/components/common/wrapper";
+import SmallTitle from "@/components/common/Titles/SmallTitle";
+import FullButton from "@/components/common/buttons/FullButton";
 import spaceman from "@/assets/img/desing_spaceman.png"
 import stars from "@/assets/img/desing_stars.png"
 import waves from "@/assets/img/desing_waves.png"
-import SmallTitle from "@/components/common/Titles/SmallTitle";
 
 const DesignerTasks = () => {
     return (
@@ -23,11 +24,24 @@ const DesignerTasks = () => {
                         продумывает наиболее верные решения для подачи информации
                     </div>
                 </div>
-                <div className={style.visual}>Контент+визуал</div>
+                <div className={style.visual}>
+                    <div className={style.visualTitle}>Контент+визуал</div>
+                    <div className={style.visualDescription}>
+                        <div className={style.visualText}> Идеальное соотношение текста и графики дает грамотное
+                            визуальное представление сайта. Работаем
+                            в минимализме, акцентировано ведем пользователя сквозь текст. В наших сайтах тексты и
+                            графика не
+                            противоречат друг другу, а поддерживают концепцию, гармонично сочетаясь между собой, даже
+                            если
+                            графики «много»
+                        </div>
+                        <FullButton className={style.visualButton}>Заполнить анкету</FullButton>
+                    </div>
+                    <div className={style.background}>
+                        <img src={waves.src} alt="design" className={style.waves}/>
+                    </div>
+                </div>
             </div>
-            {/*<div className={style.background}>
-                <img src={waves.src} alt="design" className={style.waves}/>
-            </div>*/}
 
         </Wrapper>
     );
