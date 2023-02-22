2e9ae9a30e3fa62544e98c17ce2d134f15895a30
DMITRY <bdodinka1992@gmail.com>
2023-02-22 10:24:22 +0300

    fix:after PR

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -3,40 +3,40 @@ import styles from '../../../styles/components/footer.module.scss'
 import Link from "next/link";
 import Wrapper from "@/components/common/wrapper";
 import Image from 'next/image'
-import logo from '@/../public/Logo.svg'
+import logo from '@/assets/svg/Logo.svg'
 
 export const Footer: React.FC = () => {
     return (
         <footer>
-        <Wrapper className={styles.wrapper}>
-            <div className={styles.contacts}>
-                <div className={styles.logo}>
-                    <Image src={logo} alt={"silevans logo"}/>
+            <Wrapper className={styles.wrapper}>
+                <div className={styles.contacts}>
+                    <div className={styles.logo}>
+                        <Image src={logo} alt={"silevans logo"}/>
+                    </div>
+                    <Link href={'tel:+79282345678'}>+7 928 234-56-78</Link>
+                    <div className={styles.mail}>Стать клиентом или партнёром
+                        <Link href={'mailto:hello@silevans.com'}>hello@silevans.com</Link>
+                    </div>
+                    <div className={styles.mail}>Присоединиться к команде
+                        <Link href={'mailto:hr@silevans.com'}>hr@silevans.com</Link>
+                    </div>
+                    <div className={styles.date}>Ⓒ {new Date().getFullYear()} Silevans. Все права защищены</div>
                 </div>
-                <Link href={'tel:+79282345678'}>+7 928 234-56-78</Link>
-                <div className={styles.mail}>Стать клиентом или партнёром
-                    <Link href={'mailto:hello@silevans.com'}>hello@silevans.com</Link>
+                <div className={styles.allServices}>
+                    <div className={styles.services1}>
+                        <Link href={'#'}>Услуги</Link>
+                        <Link href={'#'}>Разработка</Link>
+                        <Link href={'#'}>Продвижение</Link>
+                        <Link href={'#'}>Дизайн</Link>
+                    </div>
+                    <div className={styles.services2}>
+                        <Link href={'#'}>Портфолио</Link>
+                        <Link href={'#'}>Бриф</Link>
+                        <Link href={'#'}>Вакансии</Link>
+                    </div>
+                    <div className={styles.date}>Ⓒ {new Date().getFullYear()} Silevans. Все права защищены</div>
                 </div>
-                <div className={styles.mail}>Присоединиться к команде
-                    <Link href={'mailto:hr@silevans.com'}>hr@silevans.com</Link>
-                </div>
-                <div className={styles.date}>Ⓒ {new Date().getFullYear()} Silevans. Все права защищены</div>
-            </div>
-            <div className={styles.allServices}>
-                <div className={styles.services1}>
-                    <Link href={'#'}>Услуги</Link>
-                    <Link href={'#'}>Разработка</Link>
-                    <Link href={'#'}>Продвижение</Link>
-                    <Link href={'#'}>Дизайн</Link>
-                </div>
-                <div className={styles.services2}>
-                    <Link href={'#'}>Портфолио</Link>
-                    <Link href={'#'}>Бриф</Link>
-                    <Link href={'#'}>Вакансии</Link>
-                </div>
-                <div className={styles.date}>Ⓒ {new Date().getFullYear()} Silevans. Все права защищены</div>
-            </div>
-        </Wrapper>
-    </footer>
+            </Wrapper>
+        </footer>
     );
 };
