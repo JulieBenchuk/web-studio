307bc0b03782a78c26847c7383acf99f3d3d9cf2
DMITRY <bdodinka1992@gmail.com>
2023-02-23 12:43:53 +0300

    fix:after merge IMG TAG

--- src/components/common/slider/Slider.tsx
+++ src/components/common/slider/Slider.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from '@/styles/components/slider/slider.module.scss'
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Autoplay, Navigation} from "swiper";
-import Image from 'next/image'
 import ava from '@/assets/img/reviews_ava.png'
 import arrow from '@/assets/img/Arrow_2.png'
 
@@ -33,32 +32,32 @@ export const Slider: React.FC<SwiperDataType> = ({swiperData}) => {
                     loop={true}
                     autoplay={true}
             >
-                    {swiperData.map((el, index) => {
-                        return <SwiperSlide key={index} className={styles.slide}>
-                                <div className={styles.image}>
-                                    <Image src={ava} alt={el.title}/>
+                {swiperData.map((el, index) => {
+                    return <SwiperSlide key={index} className={styles.slide}>
+                        <div className={styles.image}>
+                            <img src={ava.src} alt={el.title}/>
+                        </div>
+                        <div className={styles.allText}>
+                            <div className={styles.title}>
+                                {el.title}
+                            </div>
+                            <div className={styles.subTitle}>{el.subTitle}</div>
+                            <div className={styles.desc}>
+                                {el.desc}
+                            </div>
+                            <div className={styles.price}>{el.price && `от ${el.price} ₽`}</div>
+                            <div className={styles.buttons}>
+                                <div className={`swiper-button-prev ${styles.swiperButtonPrev}`}>
+                                    <img src={arrow.src} alt={'Arrow'}/>
                                 </div>
-                                <div className={styles.allText}>
-                                    <div className={styles.title}>
-                                        {el.title}
-                                    </div>
-                                    <div className={styles.subTitle}>{el.subTitle}</div>
-                                    <div className={styles.desc}>
-                                        {el.desc}
-                                    </div>
-                                    <div className={styles.price}>{el.price && `от ${el.price} ₽`}</div>
-                                    <div className={styles.buttons}>
-                                        <div className={`swiper-button-prev ${styles.swiperButtonPrev}`}>
-                                            <Image src={arrow} alt={'Arrow'}/>
-                                        </div>
-                                        {`0${index + 1}`}/<span>{`0${swiperData.length}`}</span>
-                                        <div className={`swiper-button-next ${styles.swiperButtonNext}`}>
-                                            <Image src={arrow} alt={'Arrow'}/>
-                                        </div>
-                                    </div>
+                                {`0${index + 1}`}/<span>{`0${swiperData.length}`}</span>
+                                <div className={`swiper-button-next ${styles.swiperButtonNext}`}>
+                                    <img src={arrow.src} alt={'Arrow'}/>
                                 </div>
-                        </SwiperSlide>
-                    })}
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                })}
             </Swiper>
             <style jsx>{`
               .slideClass {
@@ -80,8 +79,9 @@ export const Slider: React.FC<SwiperDataType> = ({swiperData}) => {
               .swiper-slide {
 
               }
-              .swiper-wrapper{
-              display: none;
+
+              .swiper-wrapper {
+                display: none;
               }
             `}</style>
         </div>
