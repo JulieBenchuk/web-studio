45566f61306c788fce2d770b45d6c530cb704031
JulieBenchuk <juliebenchuk@gmail.com>
2023-02-25 12:39:53 +0300

    feat: wrapped by ScrollElement to scroll

--- src/components/common/callBackMe/CallBackMe.tsx
+++ src/components/common/callBackMe/CallBackMe.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SmallTitle from "@/components/common/Titles/SmallTitle";
 import Wrapper from "@/components/common/wrapper";
-import style from './../../../styles/components/callBackMe.module.scss'
 import FullButton from "@/components/common/buttons/FullButton";
 import {useFormik} from "formik";
 import * as yup from "yup";
+import * as Scroll from 'react-scroll';
+import style from './../../../styles/components/callBackMe.module.scss'
+
 
 const CallBackMe = () => {
+
+    const {Element: ScrollElement} = Scroll;
 
     const formik = useFormik({
         initialValues: {
@@ -23,30 +27,35 @@ const CallBackMe = () => {
         })
     })
     return (
-        <Wrapper className={style.wrapper}>
-            <SmallTitle children={"Остались вопросы?"} className={style.title}/>
-            <form className={style.form} onSubmit={formik.handleSubmit}>
+        <ScrollElement
+            id="callMeBack"
+            name="callMeBack"
+        >
+            <Wrapper className={style.wrapper}>
+                <SmallTitle children={"Остались вопросы?"} className={style.title}/>
+                <form className={style.form} onSubmit={formik.handleSubmit}>
 
-                <div className={style.column1}>
-                    {formik.errors.name && formik.touched.name &&
-                        <span className={style.formikError}>{formik.errors.name}</span>}
-                    <input name={"name"} type="text" value={formik.values.name} placeholder="Иванов Иван Иванович"
-                           className={style.name} onChange={formik.handleChange} onBlur={formik.handleBlur}
-                    />
-                    <p className={style.description}>Оставьте свои контакты и наши специалисты с вами свяжутся</p>
-                </div>
+                    <div className={style.column1}>
+                        {formik.errors.name && formik.touched.name &&
+                            <span className={style.formikError}>{formik.errors.name}</span>}
+                        <input name={"name"} type="text" value={formik.values.name} placeholder="Иванов Иван Иванович"
+                               className={style.name} onChange={formik.handleChange} onBlur={formik.handleBlur}
+                        />
+                        <p className={style.description}>Оставьте свои контакты и наши специалисты с вами свяжутся</p>
+                    </div>
 
-                <div className={style.column2}>
-                    {formik.errors.phone && formik.touched.phone &&
-                        <span className={style.formikError}>{formik.errors.phone}</span>}
-                    <input type="tel" name={"phone"} value={formik.values.phone} placeholder="+7 (___) ___ __ __"
-                           className={style.phone} onChange={formik.handleChange} onBlur={formik.handleBlur}
-                    />
-                    <FullButton className={style.button} type={"submit"}>Заказать звонок</FullButton>
-                </div>
+                    <div className={style.column2}>
+                        {formik.errors.phone && formik.touched.phone &&
+                            <span className={style.formikError}>{formik.errors.phone}</span>}
+                        <input type="tel" name={"phone"} value={formik.values.phone} placeholder="+7 (___) ___ __ __"
+                               className={style.phone} onChange={formik.handleChange} onBlur={formik.handleBlur}
+                        />
+                        <FullButton className={style.button} type={"submit"}>Заказать звонок</FullButton>
+                    </div>
 
-            </form>
-        </Wrapper>
+                </form>
+            </Wrapper>
+        </ScrollElement>
     );
 };
 
