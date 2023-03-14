30dbd9e4201015a89e380f5856801800c3317568
JulieBenchuk <juliebenchuk@gmail.com>
2023-03-15 01:56:29 +0300

    refactor:  next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const nextConfig = {
         loader: 'akamai',
         path: './',
     },
+    env: {
+        TOKEN: process.env.TOKEN,
+        CHAT_ID: process.env.CHAT_ID
+    }
 };
 
 export default nextConfig;
