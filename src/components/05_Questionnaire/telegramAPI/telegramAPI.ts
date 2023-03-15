7c2ae111f027915d015fe50f8f2f4629b2e3fd2d
JulieBenchuk <juliebenchuk@gmail.com>
2023-03-15 23:18:31 +0300

    refactor: NEXT_PUBLIC_TOKEN

--- src/components/05_Questionnaire/telegramAPI/telegramAPI.ts
+++ src/components/05_Questionnaire/telegramAPI/telegramAPI.ts
@@ -1,3 +1,3 @@
 export const TOKEN = process.env.TOKEN
 export const CHAT_ID = process.env.CHAT_ID
-export const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+export const URI_API = `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TOKEN}/sendMessage`
