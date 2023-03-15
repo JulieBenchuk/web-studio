50f30c85eb93171a917fe150f7b6148f6f056a33
JulieBenchuk <juliebenchuk@gmail.com>
2023-03-15 23:39:42 +0300

    refactor: .gitignore

--- src/components/05_Questionnaire/telegramAPI/telegramAPI.ts
+++ src/components/05_Questionnaire/telegramAPI/telegramAPI.ts
@@ -1,3 +1,3 @@
-export const TOKEN = process.env.NEXT_PUBLIC_TOKEN
-export const CHAT_ID = process.env.NEXT_PUBLIC_CHAT_ID
-export const URI_API = `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TOKEN}/sendMessage`
+export const TOKEN = process.env.TOKEN
+export const CHAT_ID = process.env.CHAT_ID
+export const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
